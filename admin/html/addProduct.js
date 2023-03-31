f6fbe01d1ed332de7622db2a96df401391755977
Digvijay-Sinh <work.digvijaysinh@gmail.com>
2023-03-31 15:09:36 +0530

    image functionality added

--- admin/html/addProduct.js
+++ admin/html/addProduct.js
@@ -1,18 +1,76 @@
 const form = document.querySelector('#productForm');
-const imageData = document.querySelector('#imageString');
+// import { v4 as uuidv4 } from 'uuid';
 
+// let myuuid = uuidv4();
+
+const id = Date.now()
+
+const addImagesButton = document.getElementById('addImages');
+
+// Add an onclick event listener
+addImagesButton.addEventListener('click', function() {
+    console.log(id);
+    // Call the addImage function when the button is clicked
+    addImage()
+});
+
+function addImage() {
+    // event.preventDefault();
+    const form = document.getElementById('addImageForm');
+    const formData = new FormData();
+
+    const files = document.querySelector('[name="image"]').files;
+    // console.log(files);
+
+    // Append each file to the FormData object
+    for (let i = 0; i < files.length; i++) {
+        // console.log(i);
+        formData.append(`images`, files[i]);
+        // console.log(files[i]);
+    }
+    formData.append('productId', id);
+
+    // for (let index = 0; index < files.length; index++) {
+    //     const element = formData.get(`images[${index}]`);
+    //     console.log(element);
+
+    // }
+
+    // const data = formData.get('images')
+    // const pid = formData.get('productId')
+    // console.log(pid);
+
+    // console.log(formData.productId);
+    // console.log(formData);
+    // Send the FormData object to the server using Axios
+    axios.post('http://localhost:5000/upload', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    }).then(response => {
+        console.log(response.data);
+
+    }).catch(error => {
+        console.error(error);
+    });
+}
+// Get all the files from the file input field// Get all the files from the file input field
+
+// console.out('Your UUID is: ' + myuuid);
 
 form.addEventListener('submit', (event) => {
+
     event.preventDefault(); // prevent form from submitting
-
+    console.log(id);
     const formData = new FormData(form);
-
+    const product_id = id;
     const name = formData.get('name');
     const description = formData.get('description');
     const price = formData.get('price');
     const category_id = formData.get('category_id');
-    const image_url = imageData.value;
+    const image_url = "";
     const data = {
+        product_id,
         name,
         description,
         price,
